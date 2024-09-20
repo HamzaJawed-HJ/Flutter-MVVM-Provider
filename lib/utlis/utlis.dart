97fae81a8c054cb1873df2e1885264699dd67a92
Hamza Jawed <hamzajawed2324@gmail.com>
2024-09-20 20:48:18 +0500

    login screen work  addded

--- lib/utlis/utlis.dart
+++ lib/utlis/utlis.dart
@@ -1,4 +1,27 @@
-class Utiles{
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
-  
+class Utiles {
+  static fieldfocusNode(
+      BuildContext context, FocusNode currentFocus, FocusNode nextFocus) {
+    currentFocus.unfocus();
+    FocusScope.of(context).requestFocus(nextFocus);
+  }
+
+  static toastMessage(String message) {
+    Fluttertoast.showToast(
+      msg: message,
+      backgroundColor: Colors.black,
+      textColor: Colors.white,
+    );
+  }
+
+  static snackBar(String message, BuildContext context) {
+    return ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        backgroundColor: Colors.red,
+        content: Text(message),
+      ),
+    );
+  }
 }
--- lib/views/loginScreen.dart
+++ lib/views/loginScreen.dart
@@ -1,25 +1,57 @@
 import 'package:flutter/material.dart';
 import 'package:mvvm_project/utlis/routes/AppRoutes.dart';
 import 'package:mvvm_project/utlis/routes/routesName.dart';
+import 'package:mvvm_project/utlis/utlis.dart';
 
 class Loginscreen extends StatelessWidget {
-  const Loginscreen({super.key});
+  Loginscreen({super.key});
+
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+
+  FocusNode emailFocusNode = FocusNode();
+  FocusNode passwordFocusNode = FocusNode();
+
+  ValueNotifier<bool> _obsecurePassword = ValueNotifier<bool>(true);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Login Screen"),
+        centerTitle: true,
       ),
       body: Column(
         children: [
-          InkWell(
-              onTap: () {
-                Navigator.pushNamed(context, RoutesName.homeScreen);
-              },
-              child: Center(
-                child: Text("click Me"),
-              ))
+          TextFormField(
+            controller: _emailController,
+            focusNode: emailFocusNode,
+            decoration: InputDecoration(hintText: 'Email'),
+            onFieldSubmitted: (value) {
+              Utiles.fieldfocusNode(context, emailFocusNode, passwordFocusNode);
+            },
+          ),
+
+          TextFormField(
+            obscureText: _obsecurePassword.value,
+            controller: _passwordController,
+            focusNode: passwordFocusNode,
+            obscuringCharacter: '*',
+            decoration: InputDecoration(
+              hintText: 'Password',
+              suffixIcon: Icon(Icons.visibility_off_outlined),
+            ),
+          ),
+
+          // InkWell(
+          //   onTap: () {
+          //     //  Navigator.pushNamed(context, RoutesName.homeScreen);
+          //     Utiles.snackBar("message", context);
+          //   },
+          //   child: Center(
+          //     child: Text("click Me"),
+          //   ),
+          // )
         ],
       ),
     );
